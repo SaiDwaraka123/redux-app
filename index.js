a25e9e98616cfcb39f434180238989f3ee401128
SaiDwaraka123 <148796717+SaiDwaraka123@users.noreply.github.com>
2024-07-30 23:08:15 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+export const increase = ()=> ({
+    type:"Increment",
+});
 
-import App from './App';
-import { Provider } from 'react-redux';
-import store from './store';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-    <App />
-    </Provider>
-    
-  </React.StrictMode>
-);
-
-
+export const decrease = ()=> ({
+    type:"Decrement",
+});
